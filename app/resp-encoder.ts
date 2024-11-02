48c6113eed8d8329c49640fde4ebc4a55107a259
ghostkode <jayakrishnan1012@gmail.com>
2024-11-02 23:55:56 +0530

    codecrafters submit [skip ci]

--- app/resp-encoder.ts
+++ app/resp-encoder.ts
@@ -1,20 +1,55 @@
 enum RESPCommands {
   PING = "PING",
   ECHO = "ECHO",
+  SET = "SET",
+  GET = "GET",
 }
 
 export class RESPEncoder {
+  private store: Map<string, string> = new Map();
+
+  constructor() {
+    this.store = new Map();
+  }
+
   encode(data: string[]): string {
     const cmd = data[0];
+    const args = data.slice(1);
 
     switch (cmd.toUpperCase()) {
       case RESPCommands.PING:
         return this.serialize("PONG");
       case RESPCommands.ECHO:
-        return this.serialize(data[1]);
+        return this.serialize(args.join(" "));
+      case RESPCommands.SET:
+        return this.cmdSet(args);
+      case RESPCommands.GET:
+        return this.cmdGet(args);
       default:
         return this.serialize(new Error("Unknown command"));
     }
+  }
+
+  private cmdSet(data: string[]): string {
+    if (data.length !== 2) {
+      return this.serialize(new Error("SET command requires 2 arguments"));
+    }
+
+    const key = data[0];
+    const value = data[1];
+
+    this.store.set(key, value);
+    return this.serialize("OK");
+  }
+
+  private cmdGet(data: string[]): string {
+    if (data.length !== 1) {
+      return this.serialize(new Error("GET command requires 1 argument"));
+    }
+    const key = data[0];
+    const value = this.store.get(key) ?? null;
+
+    return this.serialize(value);
   }
 
   private serialize(data: any): string {
